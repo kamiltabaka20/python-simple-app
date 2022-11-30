4b64077013a6cddf2697c50377c55f0a65894940
kamiltabaka20 <kamil.tabaka20@gmail.com>
2022-11-30 19:53:55 +0100

    Added draw number function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def check_name(name):
     if name.isalpha():
         print("Hello {0}!".format(name.capitalize()))
@@ -10,3 +13,13 @@ def check_name(name):
 user_name = check_name(input("What's your name?\n"))
 while not user_name:
     user_name = check_name(input("Try again, please.\n"))
+
+
+def draw_number():
+    number = randint(1, 100)
+    print("Today, your lucky number is {0}!".format(number))
+    return number
+
+
+user_number = draw_number()
+
